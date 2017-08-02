ff71fce599ea7292621403155720cd68c8c8b0b1
ALexander4295502 <yuanzhengstl@gmail.com>
2017-08-01 23:44:23 -0500

    Fix bugs on parts with angular material

--- src/app/shared/layout/sidebar.component.ts
+++ src/app/shared/layout/sidebar.component.ts
@@ -3,7 +3,7 @@ import { Component, HostBinding, OnInit, ViewEncapsulation } from '@angular/core
 @Component({
     selector: 'layout-sidebar',
     templateUrl: './sidebar.component.html',
-    styleUrls: ['./sidebar.component.css'],
+    styleUrls: ['./sidebar.component.css',],
     encapsulation: ViewEncapsulation.Native
 })
 export class SidebarComponent implements OnInit{
